e50b4b12ef85fb556265277b00284a8888f292a5
Koadithekingslayer <cbwagar@gmail.com>
2024-07-23 16:51:50 -0400

    Final Edits to comply with FreeCodeCamp requirements.

--- Calculators/arithmeticArranger.py
+++ Calculators/arithmeticArranger.py
@@ -36,10 +36,12 @@ def arranger_output(number_1, op, number_2, answers, number_problems):
     
     row_2 += op + (' ' * (spacing - 1 - len(number_2))) + number_2 
     dashes += '-' * spacing
+    
     global first_row
     global second_row
     global third_row
     global fourth_row
+    
     if counter < number_problems:
         first_row += row_1 + '    '
         second_row += row_2 + '    '
@@ -52,40 +54,46 @@ def arranger_output(number_1, op, number_2, answers, number_problems):
         fourth_row += row_4       
 
 def arithmetic_arranger(problems, show_answers=False):
+    global first_row, second_row, third_row, fourth_row, counter
+    
+    # Reset global rows and counter at the beginning of each call
+    first_row = ''
+    second_row = ''
+    third_row = ''
+    fourth_row = ''
+    counter = 0
+
     # If more than 5 Problems entered, Error.
     if len(problems) > 5:
-        raise RuntimeError('Error: Too Many Problems.')
+        return('Error: Too many problems.')
 
-    # check operators
+    # Check operators
     if not all(('+' in problem or '-' in problem) for problem in problems):
-        raise RuntimeError("Error: Operator must be '+' or '-'.")
+        return("Error: Operator must be '+' or '-'.")
 
-    # verify and collect numbers
+    # Verify and collect numbers and operators
     numbers = [] # operands as strings
     operators = []
-    for i in problems:
-        collection = i.split()
-        if len(collection[0]) > 4 or len(collection[2]) > 4:
-            raise RuntimeError('Error: Numbers can only be 4 digits long.')
-        else:
-            numbers.append(collection[0])
-            numbers.append(collection[2])
-            operators.append(collection[1])
-    if not all(map(lambda x: x.isdigit(), numbers)):
-        raise RuntimeError('Error: Numbers must contain only digits.')
+    for problem in problems:
+        parts = problem.split()
+        if len(parts[0]) > 4 or len(parts[2]) > 4:
+            return('Error: Numbers cannot be more than four digits.')
+        if not (parts[0].isdigit() and parts[2].isdigit()):
+            return('Error: Numbers must only contain digits.')
+        
+        numbers.append(parts[0])
+        numbers.append(parts[2])
+        operators.append(parts[1])
+
+    iterations = len(operators)
     
-    iterations = len(numbers) / 2
-    
-    for iter in range(int(iterations)):
-        arranger_output(numbers[iter], operators[iter], numbers[iter + 1 * 2], show_answers, len(numbers) / 2)
-        if iter >= iterations - 1:
-            break
-    
+    for i in range(iterations):
+        arranger_output(numbers[i * 2], operators[i], numbers[i * 2 + 1], show_answers, iterations)
+
     final_string = first_row + '\n' + second_row +'\n' + third_row
     if show_answers:
         final_string += '\n' + fourth_row
-    return  final_string
-
+    return final_string
 
 input = ["3801 - 2", "123 + 49"]
 print(arithmetic_arranger(input))
